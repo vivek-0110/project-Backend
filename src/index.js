5b2ed5ae022b07206726553833b9f50dfed00014
Vivek Kumar Dubey <vivekdubeyvd0110@gmail.com>
2024-06-12 16:18:14 +0530

    middleware setup and utils setup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then( () => {
+    
+        app.on("error", (error) => {
+            console.log("ERROR: ", error);
+            throw error;
+           });
+    
+
+        app.listen(process.env.PORT, () => {
+        console.log(`App is listening on port ${process.env.PORT}`);
+        });
+})
+.catch( (err) => {
+    console.log("MongoDb connection failed !!", err);
+
+})
 
 
 
